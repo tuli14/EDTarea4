56c5f306039040b75282ec66b6726bfce058a773
Alba <albitax.94@gmail.com>
2022-03-18 10:47:48 +0100

    Se han cambiado los valores proporcionados en el proyecto deposito dentro del método operativa_cuenta por el parámetro cantidad

--- deposito/src/Cuentas/Main.java
+++ deposito/src/Cuentas/Main.java
@@ -16,20 +16,20 @@ public class Main {
 
         Cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
         saldoActual = Cuenta1.estado();
-        System.out.println("El saldo actual es"+ saldoActual );
+        System.out.println("El saldo actual es "+ saldoActual );
 
         operativa_cuenta(Cuenta1, 200);
     }
 
     public static void operativa_cuenta(CCuenta Cuenta1, float cantidad) {
         try {
-            Cuenta1.retirar(2300);
+            Cuenta1.retirar(cantidad);
         } catch (Exception e) {
             System.out.print("Fallo al retirar");
         }
         try {
             System.out.println("Ingreso en cuenta");
-            Cuenta1.ingresar(695);
+            Cuenta1.ingresar(cantidad);
         } catch (Exception e) {
             System.out.print("Fallo al ingresar");
         }
